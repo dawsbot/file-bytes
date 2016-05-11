27fc4f3d7f609a7c384f6dba471bef4e9d080a39
Dawson Botsford <DawsonBotsford@gmail.com>
2016-05-10 23:06:52 -0600

    implement async in promise

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 'use strict';
 const fs = require('fs');
-const pathExists = require('path-exists');
+const pify = require('pify');
 
-module.exports = filename => new Promise((resolve, reject) => {
-  if (pathExists.sync(filename)) {
-    const stats = fs.statSync(filename);
-    resolve(stats.size);
-  }
-
-  reject(new Error(`File "${filename}" not found`));
-});
+module.exports = filename => {
+  return pify(fs.stat)(filename)
+    .then(stats => stats.size)
+    .catch(err => {
+      throw err;
+    });
+};
 
 module.exports.sync = filename => {
-  if (pathExists.sync(filename)) {
+  try {
     const stats = fs.statSync(filename);
     return stats.size;
+  } catch (err) {
+    throw err;
   }
-  throw new Error(`File "${filename}" not found`);
 };
--- test.js
+++ test.js
@@ -3,42 +3,47 @@ const sh = require('shelljs');
 import test from 'ava';
 import fileBytes from './';
 
+function getRand() {
+  const min = 0;
+  const max = 99999999999999999999;
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
 test('invalid args sync', t => {
   t.throws(() => {
     fileBytes.sync('invalidFileName.txt');
-  }, Error);
+  }, /ENOENT/);
 });
 
 test('empty file sync', t => {
-  sh.touch('empty.txt');
-  t.is(fileBytes.sync('empty.txt'), 0);
-  t.is(typeof fileBytes.sync('empty.txt'), 'number');
-  sh.rm('empty.txt');
+  const file = `empty${getRand()}.txt`;
+  sh.touch(file);
+  t.is(fileBytes.sync(file), 0);
+  t.is(typeof fileBytes.sync(file), 'number');
+  sh.rm(file);
 });
 
 test('small file sync', t => {
-  fs.writeFileSync('small.txt', '0123456789');
-  t.is(fileBytes.sync('small.txt'), 10);
-  sh.rm('small.txt');
+  const file = `small${getRand()}.txt`;
+  fs.writeFileSync(file, 'four');
+  t.is(fileBytes.sync(file), 4);
+  sh.rm(file);
 });
 
-test('invalid args promise', async t => {
-  try {
-    await fileBytes('invalidFileName.txt');
-    t.fail('Exception was not thrown');
-  } catch (err) {
-    t.truthy(err);
-  }
+test('invalid args promise', t => {
+  t.throws(fileBytes('invalidFileName'), /ENOENT/);
 });
 
 test('empty file promise', async t => {
-  sh.touch('empty.txt');
-  t.is(await fileBytes('empty.txt'), 0);
-  sh.rm('empty.txt');
+  const file = `empty${getRand()}.txt`;
+  sh.touch(file);
+  t.is(await fileBytes(file), 0);
+  sh.rm(file);
 });
 
 test('small file promise', async t => {
-  fs.writeFileSync('small.txt', '0123456789');
-  t.is(await fileBytes('small.txt'), 10);
-  sh.rm('small.txt');
+  const file = `small${getRand()}.txt`;
+  fs.writeFileSync(file, 'four');
+  t.is(await fileBytes(file), 4);
+  sh.rm(file);
 });
